e6a305ff845cadad3947bef839429382b2770b5f
Peter Bartha <mail@peterbartha.com>
2020-09-20 01:59:05 +0200

    chore: configure eslint, prettier and husky

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
 /**
- * Configure your Gatsby site with this file.
+ * Configure Gatsby with this file.
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
+};
